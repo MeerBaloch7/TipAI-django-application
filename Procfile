688009dcc30ab11a7c0a749886763cd9940f4a07
example_name <example_password@gmail.com>
2025-09-03 15:57:57 +0500

    Remove all Heroku-related files and configurations

--- Procfile
+++
@@ -1,2 +0,0 @@
-release: bash release.sh
-web: cd tip_predictor && gunicorn tip_predictor.wsgi --log-file -
--- release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Heroku release script
-cd tip_predictor
-python manage.py migrate --noinput
